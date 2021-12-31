c835d97e5d320dfe8d451e582a2131d6197c83fe
coder_Q <coderq@qq.com>
2021-12-31 16:47:34 +0800

    feat: ✨再次添加span

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,7 @@ function App() {
   return (
     <div className="App">
       上传需求
-<<<<<<< HEAD
       <span>再次修改</span>
-=======
-      这个才是正确的
->>>>>>> 226be794326c6a3a115d25c9f307a2fbeaca5519
     </div>
   )
 }
